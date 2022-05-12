86a3a890271560a54c7ec92e6e01592f05097815
Thomas Burns <burns235577@gmail.com>
2022-05-12 11:15:03 -0500

    Finnished adding the comments to the code

--- Q's project folder/frontend.py
+++ Q's project folder/frontend.py
@@ -7,7 +7,7 @@ import requests
 with open('cryptocurrencies.json', 'r') as file:
     data = json.loads(file.read())
     
-#Defined the function that (TO BE CONTINUED)
+#Defined the function "get_price" in order to accept the name parameter. Then we made several variables inside the function. In the "base_url" varaiable we referenced the endpoint url in order to seek information on certain cryptos. The "url" variable uses the endpoint url and the name of the crypto to convert into USD value. The "response" variable uses the get function to request data from the server. The "key" variable gets the keys from the dictionary in the json file. The "price" variable puts them all together in order to get the end product which we then return the value of.
 def get_price(name):
     base_url = 'https://api.alternative.me/v2/ticker/'
     url = base_url + name + '/?convert=USD'
@@ -16,7 +16,7 @@ def get_price(name):
     price = response['data'][key]['quotes']['USD']['price']
     return price
 
-#(TO BE CONTINUED)
+#We defined the function "convert" in order to accept the parameters (amount, coin1, and coin2). This second defined function is used to convert the value of the first coin into the value of the second coin. The price of the cryptocurrency is retrieved but using the "get_price" function above. Then the "value_usd1" variable takes the price of the first coin and multiplies it by the amount the user has. The variable "value_usd2" takes the value from "value_usd1" and divides it by the variable "coin2_price". The result of "value_usd2" is returned. 
 def convert(amount, coin1, coin2):
     coin1_price = get_price(coin1)
     coin2_price = get_price(coin2)
@@ -32,7 +32,7 @@ coin1_input = st.text_input('Choose first crypto', 'BTC')
 coin2_input = st.text_input('Choose second crypto', 'ETH')
 amount = st.slider('Choose the amount', 1,100)
 
-#Created two more variables for when the user inputs his own data. The converter will use the users given data to return the value instead of the default inputs given before.
+#Created two more variables for when the user inputs his own data. The converter will use the users given data to return the value instead of the default inputs given before when the Currency Converter first launched.
 coin1 = data[coin1_input]
 coin2 = data[coin2_input]
 st.write(convert(amount, coin1.lower().replace(" ", "-"), coin2.lower().replace(" ", "-")))
